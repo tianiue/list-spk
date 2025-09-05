c1598975980763378bd16b8ddf273dcac83f44c9
tianiue <61725402+tianiue@users.noreply.github.com>
2025-09-05 15:20:11 +0800

    Update start-stop-status.sh

--- start-stop-status.sh
+++ start-stop-status.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
+MOUNT_POINT=$(cat /var/packages/OpenListMount/etc/mount_point || echo "/volume1/OpenList")
 
 case $1 in
     start)
-        MOUNT_POINT=$(cat /var/packages/OpenListMount/etc/mount_point)
-        mkdir -p "$MOUNT_POINT"
-        
         if [ -f /var/packages/OpenListMount/etc/api_token ]; then
             rclone mount openlist:/ "$MOUNT_POINT" \
               --webdav-header "Authorization: Bearer $(cat /var/packages/OpenListMount/etc/api_token)" \
@@ -21,11 +19,9 @@ case $1 in
         fi
         ;;
     stop)
-        MOUNT_POINT=$(cat /var/packages/OpenListMount/etc/mount_point)
         fusermount -u "$MOUNT_POINT"
         ;;
     status)
-        MOUNT_POINT=$(cat /var/packages/OpenListMount/etc/mount_point)
         mountpoint -q "$MOUNT_POINT" && echo "Running" || echo "Stopped"
         ;;
 esac
